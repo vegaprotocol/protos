e85b957040b2fb968b087cc93b3329076c00c7f2
Karel Moravec <moravec.wdd@gmail.com>
2021-08-25 18:47:02 +0200

    add delegations

--- data-node/api/v1/trading_data.pb.gw.go
+++ data-node/api/v1/trading_data.pb.gw.go
@@ -1269,6 +1269,23 @@ func request_TradingDataService_GetRewardDetails_0(ctx context.Context, marshale
 
 }
 
+var (
+	filter_TradingDataService_Delegations_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
+func request_TradingDataService_Delegations_0(ctx context.Context, marshaler runtime.Marshaler, client TradingDataServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq DelegationsRequest
+	var metadata runtime.ServerMetadata
+
+	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_TradingDataService_Delegations_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.Delegations(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 func request_TradingDataService_PartyStake_0(ctx context.Context, marshaler runtime.Marshaler, client TradingDataServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq PartyStakeRequest
 	var metadata runtime.ServerMetadata
@@ -2334,6 +2351,26 @@ func RegisterTradingDataServiceHandlerClient(ctx context.Context, mux *runtime.S
 
 	})
 
+	mux.Handle("GET", pattern_TradingDataService_Delegations_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_TradingDataService_Delegations_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_TradingDataService_Delegations_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("GET", pattern_TradingDataService_PartyStake_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -2458,6 +2495,8 @@ var (
 
 	pattern_TradingDataService_GetRewardDetails_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 1, 2, 2}, []string{"parties", "party_id", "rewards"}, ""))
 
+	pattern_TradingDataService_Delegations_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{"delegations"}, ""))
+
 	pattern_TradingDataService_PartyStake_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 1, 2, 2}, []string{"parties", "party", "stake"}, ""))
 )
 
@@ -2561,6 +2600,8 @@ var (
 	forward_TradingDataService_OracleDataBySpec_0 = runtime.ForwardResponseMessage
 
 	forward_TradingDataService_GetRewardDetails_0 = runtime.ForwardResponseMessage
+
+	forward_TradingDataService_Delegations_0 = runtime.ForwardResponseMessage
 
 	forward_TradingDataService_PartyStake_0 = runtime.ForwardResponseMessage
 )
