ba214c3cd0897294d68fd513e451c71acedb7e34
jeremyletang <letang.jeremy@gmail.com>
2021-09-06 08:39:34 +0100

    remove unused import + regen

--- vega/api/trading.validator.pb.go
+++ vega/api/trading.validator.pb.go
@@ -10,9 +10,7 @@ import (
 	_ "code.vegaprotocol.io/protos/vega"
 	_ "code.vegaprotocol.io/protos/vega/commands/v1"
 	_ "code.vegaprotocol.io/protos/vega/events/v1"
-	_ "code.vegaprotocol.io/protos/vega/oracles/v1"
 	proto "github.com/golang/protobuf/proto"
-	_ "github.com/mwitkow/go-proto-validators"
 	github_com_mwitkow_go_proto_validators "github.com/mwitkow/go-proto-validators"
 )
 
