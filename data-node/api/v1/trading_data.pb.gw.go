0359e9c6df2375a4aa6293b2165978b5c5763eed
Karel Moravec <moravec.wdd@gmail.com>
2021-09-06 14:13:49 +0100

    move last block height to proxy

--- data-node/api/v1/trading_data.pb.gw.go
+++ data-node/api/v1/trading_data.pb.gw.go
@@ -909,15 +909,6 @@ func request_TradingDataService_GetEpoch_0(ctx context.Context, marshaler runtim
 
 }
 
-func request_TradingDataService_LastBlockHeight_0(ctx context.Context, marshaler runtime.Marshaler, client TradingDataServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq LastBlockHeightRequest
-	var metadata runtime.ServerMetadata
-
-	msg, err := client.LastBlockHeight(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
-	return msg, metadata, err
-
-}
-
 func request_TradingDataService_GetVegaTime_0(ctx context.Context, marshaler runtime.Marshaler, client TradingDataServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq GetVegaTimeRequest
 	var metadata runtime.ServerMetadata
@@ -2011,26 +2002,6 @@ func RegisterTradingDataServiceHandlerClient(ctx context.Context, mux *runtime.S
 
 	})
 
-	mux.Handle("GET", pattern_TradingDataService_LastBlockHeight_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateContext(ctx, mux, req)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := request_TradingDataService_LastBlockHeight_0(rctx, inboundMarshaler, client, req, pathParams)
-		ctx = runtime.NewServerMetadataContext(ctx, md)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-
-		forward_TradingDataService_LastBlockHeight_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
-	})
-
 	mux.Handle("GET", pattern_TradingDataService_GetVegaTime_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -2461,8 +2432,6 @@ var (
 
 	pattern_TradingDataService_GetEpoch_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 1}, []string{"epochs", "id"}, ""))
 
-	pattern_TradingDataService_LastBlockHeight_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"blockchain", "height"}, ""))
-
 	pattern_TradingDataService_GetVegaTime_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{"time"}, ""))
 
 	pattern_TradingDataService_AssetByID_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 1}, []string{"assets", "id"}, ""))
@@ -2566,8 +2535,6 @@ var (
 	forward_TradingDataService_GetNodeByID_0 = runtime.ForwardResponseMessage
 
 	forward_TradingDataService_GetEpoch_0 = runtime.ForwardResponseMessage
-
-	forward_TradingDataService_LastBlockHeight_0 = runtime.ForwardResponseMessage
 
 	forward_TradingDataService_GetVegaTime_0 = runtime.ForwardResponseMessage
 
