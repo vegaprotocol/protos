d7a799a0886614fcd7dd7a93e934bcd05f1438fc
Karel Moravec <moravec.wdd@gmail.com>
2021-11-11 14:10:28 +0000

    fix tests

--- commands/key_rotate_submission_test.go
+++ commands/key_rotate_submission_test.go
@@ -23,13 +23,6 @@ func TestKeyRotateSubmissionSubmittingEmptyCommandFails(t *testing.T) {
 	assert.Contains(t, err.Get("key_rotate_submission.key_number"), commands.ErrIsRequired)
 	assert.Contains(t, err.Get("key_rotate_submission.time"), commands.ErrNotAValidInteger)
 }
-func TestKeyRotateSubmissionSubmittingEmptyCommandFails(t *testing.T) {
-	err := checkKeyRotateSubmission(&commandspb.KeyRotateSubmission{})
-
-	assert.Contains(t, err.Get("key_rotate_submission.new_pub_key_hash"), commands.ErrIsRequired)
-	assert.Contains(t, err.Get("key_rotate_submission.key_number"), commands.ErrIsRequired)
-	assert.Contains(t, err.Get("key_rotate_submission.time"), commands.ErrNotAValidInteger)
-}
 
 func TestKeyRotateSubmissionMissingNewPubKeyHashFails(t *testing.T) {
 	err := checkKeyRotateSubmission(&commandspb.KeyRotateSubmission{
