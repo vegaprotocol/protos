81738ab3611a6c19e3f2aba2eaec9f034c2fa877
Peter Barrow <pete@vega.xyz>
2022-03-15 11:37:23 +0000

    fix: removed whitespace that was breaking the script

--- script/gettools.sh
+++ script/gettools.sh
@@ -9,8 +9,8 @@ google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2.0
 github.com/chrusty/protoc-gen-jsonschema/cmd/protoc-gen-jsonschema@latest
 github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@v2.7.3
 github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@v2.7.3
-github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@v1.5.1
-"
+github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@v1.5.1"
+
 	# Note: Make sure the above tools and versions match the ones in devops-infra/docker/cipipeline/Dockerfile
 	echo "$tools" | while read -r toolurl ; do
 		go install "$toolurl"
